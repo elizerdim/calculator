62ca5ee9e2c7ce14e582e11f2568bf2a34149e48
Eliz Erdim <elizerdim@gmail.com>
2024-03-14 21:20:46 +0300

    Add digit and operator buttons functionality

---
+++ script.js
@@ -0,0 +1,87 @@
+const buttonsDiv = document.querySelector('.buttons');
+const displayContent = document.querySelector('.display-content');
+
+let firstOperand;
+let secondOperand;
+let operator;
+let result;
+
+let displayIsToBeCleared = false;
+let operatorClicked = false;
+let newOperandOnDisplay = false;
+
+buttonsDiv.addEventListener('click', handleClick);
+// window.addEventListener('keydown', handleKeydown);
+
+function handleClick(e) {
+  if (e.target.classList.value.includes('digit')) {
+    handleDigits(e);
+  } else if (e.target.classList.value.includes('operator')) {
+    handleOperators(e);
+  } 
+}
+
+function handleDigits(e) {
+  if (displayIsToBeCleared && operatorClicked) {
+    displayContent.innerText = '';
+  }
+
+  displayIsToBeCleared = false;
+  operatorClicked = false;
+  newOperandOnDisplay = true;
+  displayContent.innerText += e.target.innerText;
+}
+
+function handleOperators(e) {
+  if (operatorClicked) {
+    operator = e.target.innerText;
+    return;
+  }
+
+  if (!!firstOperand && !!operator) {
+    secondOperand = displayContent.innerText;
+    operate(firstOperand, secondOperand, operator);
+    displayContent.innerText = result;
+  } 
+
+  firstOperand = displayContent.innerText;
+  operator = e.target.innerText;
+
+  displayIsToBeCleared = true;
+  operatorClicked = true;
+  newOperandOnDisplay = false;
+}
+
+function operate(operand1, operand2, operator) {
+  //carry out the operation
+  switch (operator) {
+    case '+':
+      result = +operand1 + +operand2;
+      break;
+    case '-':
+      result = +operand1 - +operand2;
+      break;
+    case 'x':
+      result = +operand1 * +operand2;
+      break;
+    case '/':
+      if (+operand2 === 0) {
+        result = 'Nice try!';
+      }
+      result = +operand1 / +operand2;
+      break;
+  }
+
+  //format the result to have no more than 9 characters, including negative sign and decimal point
+  let resultStr = result.toString();
+  
+  if (resultStr.length > 9) {
+    if (resultStr.includes('-') && result.Str.includes('.')) {
+      result = result.toPrecision(7).toString();
+    } else if (resultStr.includes('-') || resultStr.includes('.')) {
+      result = result.toPrecision(8).toString();
+    } else {
+      result = result.toPrecision(9).toString();
+    }
+  }
+}
